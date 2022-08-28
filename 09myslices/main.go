864d1288d03876a058dacad0db91c1bafe505fac
Hemant <hrsmart2150@gmail.com>
2022-08-28 20:41:52 +0530

    Update main.go
    Co-Authored-By: shivam danawale <70281451+shivam1317@users.noreply.github.com>
    Co-Authored-By: AdityA <adityasingla.2802@gmail.com>
    Co-Authored-By: Prabhat Sachdeva <91917879+prabhatexit0@users.noreply.github.com>
    
    Co-authored-by: shivam danawale <70281451+shivam1317@users.noreply.github.com>
    Co-authored-by: AdityA <adityasingla.2802@gmail.com>
    Co-authored-by: Prabhat Sachdeva <91917879+prabhatexit0@users.noreply.github.com>

--- 09myslices/main.go
+++ 09myslices/main.go
@@ -34,4 +34,12 @@ func main() {
 	fmt.Println("Sorted High Scores are:", highScores)
 	fmt.Println("Is highScores sorted or not?", sort.IntsAreSorted(highScores))
 
+	// how to remove a value from slices based on index
+	var courses = []string{"Docker", "Kubernetes", "Puppet", "Terraform", "AWS", "Azure", "GCP"}
+	fmt.Println("All Courses are:", courses)
+	index := 2
+	// first we sliced the slice from 0 to index and then we sliced from index+1 to end
+	courses = append(courses[:index], courses[index+1:]...) // this will remove the value at index 2
+	fmt.Println("Remaining Courses after deletion are:", courses)
+
 }
