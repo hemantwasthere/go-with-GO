4eaeeabb0715dce6501f9120f56f62a5c4c99c8f
Hemant <hrsmart2150@gmail.com>
2022-08-28 17:28:34 +0530

    Saw conversions, time function and pointers

---
+++ 07mypointers/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to a class on pointers")
+
+	// var ptr *int
+	// fmt.Println("The default value of ptr is", ptr)
+	// fmt.Printf("The type of ptr is %T", ptr)
+
+	myNumber := 23
+	var ptr = &myNumber
+	fmt.Println("Value of actual pointer is", ptr)
+	fmt.Println("Value of actual pointer is", *ptr)
+
+	*ptr = *ptr * 2
+	// *ptr *= 2 // same as above
+	fmt.Println("Value of modified pointer is", *ptr)
+}
