ec12593f75fe92493fafbbf22e07f713497bc74d
Hemant <hrsmart2150@gmail.com>
2022-09-05 01:06:15 +0530

    Created server for golang frontend

---
+++ web-server-for-golang/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.status(200).send('Welcome to LearnCodeOnline server !');
+})
+
+app.get('/get', (req, res) => {
+    res.status(200).json({ message: 'Hello from learncodeonline.in' });
+})
+
+app.post('/post', (req, res) => {
+    let myJson = req.body // your json
+    res.status(200).send(myJson);
+})
+app.post('/postform', (req, res) => {
+    res.status(200).send(JSON.stringify(req.body));
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
